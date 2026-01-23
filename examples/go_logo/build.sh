5676dfe72b08a96944078aaa1ba64c202f4f97e6
Roman Bielyi <romanbielyi.amor@gmail.com>
2026-01-23 10:37:23 +0100

    Update 'bouncing_square/build.sh' and 'go_logo/build.sh' files to include to CGO_FLAGS path to the SDK C_API pd_api.h

--- examples/go_logo/build.sh
+++ examples/go_logo/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-pdgoc -device -sim  -name=GoLogo -author=PdGo -desc="GoLogo Game" -bundle-id=com.pdgo.go_logo -version=1.0 -build-number=1
+# Set Playdate SDK path for CGO (adjust if your SDK is in a different location)
+export CGO_CFLAGS="-I$HOME/Developer/PlaydateSDK/C_API -DTARGET_EXTENSION=1"
+
+pdgoc -device -sim \
+  -name=GoLogo \
+  -author=PdGo \
+  -desc="GoLogo Game" \
+  -bundle-id=com.pdgo.go_logo \
+  -version=1.0 \
+  -build-number=1
