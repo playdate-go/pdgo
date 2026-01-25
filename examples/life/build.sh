76b439e095cf043725a2fc10a717be6ed8763142
Roman Bielyi <romanbielyi.amor@gmail.com>
2026-01-26 00:14:22 +0100

    Delete CGO_CFLAGS from all examples 'build.sh' scripts and fix 'go get'

--- examples/life/build.sh
+++ examples/life/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export CGO_CFLAGS="-I$PLAYDATE_SDK_PATH/C_API -DTARGET_EXTENSION=1"
+cd Source
 
 go get github.com/playdate-go/pdgo@latest
+
+cd ..
 
 pdgoc -device -sim \
   -name=Life \
--- examples/spritegame/build.sh
+++ examples/spritegame/build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-PLAYDATE_SDK="$PLAYDATE_SDK_PATH"
-
-# Set CGO flags for simulator build
-export CGO_CFLAGS="-I$PLAYDATE_SDK/C_API -DTARGET_EXTENSION=1"
+cd Source
 
 go get github.com/playdate-go/pdgo@latest
+
+cd ..
 
 # Copy images from SDK's Sprite Game example if not already present
 if [ ! -d "Source/images" ]; then
   echo "Copying images from Playdate SDK..."
-  cp -r "$PLAYDATE_SDK/C_API/Examples/Sprite Game/Source/images" Source/
+  cp -r "$PLAYDATE_SDK_PATH/C_API/Examples/Sprite Game/Source/images" Source/
 fi
 
 pdgoc -device -sim \
